9cebe38c2b53de9051130c576e5dee5f70047b98
Laysa Uchoa <laysa.uchoa@gmail.com>
2020-02-05 21:01:32 +0100

    clean

--- rust_types/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let a =10; //int
-    let b = 2.5; //float
-    println!("int = {} and float = {}",a,b);
-    let c:bool = false;
-    let d:bool = true;
-    println!("bool false = {} bool true = {}", c, d) 
-}
