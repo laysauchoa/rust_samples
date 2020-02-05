107f8a7b6db4246b06d9c7d37c83e19cb6170cc9
Laysa Uchoa <laysa.uchoa@gmail.com>
2020-02-06 00:40:47 +0100

    Receive the input from the user

--- recursion_rust/src/main.rs
+++ recursion_rust/src/main.rs
@@ -1,7 +1,17 @@
 mod fibonnacci;
+use std::io;
 fn main() {
-    let n = 6;
+    loop {
+        println!("Please enter the desired position for fib sequence");
+    let mut n = String::new();
+    io::stdin().read_line(&mut n)
+        .expect("Failed to read line");
+    let n: i32 = match n.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
     let fib_res = fibonnacci::fib(n);
     println!("For position n: {}", fib_res);
+}
 }
 
