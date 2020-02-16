f0fdf7eec2c85085a1792dbd5661c7b76d0481f8
Laysa Uchoa <laysa.uchoa@gmail.com>
2020-02-16 16:16:53 +0100

    add recursion rust and markup language code

---
+++ markup_language/src/main.rs
@@ -0,0 +1,37 @@
+#[derive(Copy, Clone)]
+enum State {
+    Normal,
+    Comment,
+    Upper,
+    Lower,
+}
+
+fn machine_cycle(state: State, c: char) -> (Option<char>, State) {
+    use self::State::*;
+    match (state, c) {
+        (Normal, '#') => (None, Comment),
+        (Normal, '^') => (None, Upper),
+        (Normal, '_') => (None, Lower),
+        (Normal, other) => (Some(other), Normal),
+        (Comment, '#') => (None, Normal),
+        (Comment, _) => (None, Comment),
+        (Upper, '^') => (None, Normal),
+        (Upper, other) => (Some(other.to_ascii_uppercase()), Upper),
+        (Lower, '_') => (None, Normal),
+        (Lower, other) => (Some(other.to_ascii_lowercase()), Lower),
+    }
+}
+
+fn main() {
+    let mut state = State::Normal;
+    let mut processed_string = String::new();
+    let input = "This _iS_ some ^input^. #we want this to be a comment#";
+    for character in input.chars() {
+        let (output, new_state) = machine_cycle(state, character);
+        if let Some(c) = output {
+            processed_string.push(c);
+        }
+        state = new_state;
+    }
+    println!("Processed String {}", processed_string);
+}
